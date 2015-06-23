e03c5ccf29989629bc41c6fb2cbcbb947368a908
Moufid Larbi <moufid4@gmail.com>
2015-06-23 16:00:48 +0100

    errors fix

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,20 +1,17 @@
 var postsData = [
-	{
-		title: 'Introducing Telescope',
-		author: 'Sacha Greif',
-		url: 'http://sachagreif.com/introducing-telescope/'
-	},
-	{
-		title: 'LarbiBlog',
-		author: 'Moufid Larbi',
-		url: 'http://larbibouamrane.com'
-	},
-	{
-		title: 'The Meteor Book',
-		author: 'Tom Coleman',
-		url: 'http://themeteorbook.com'
-	}
+  {
+    title: 'Introducing Telescope',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  }, 
+  {
+    title: 'Meteor',
+    url: 'http://meteor.com'
+  }, 
+  {
+    title: 'The Meteor Book',
+    url: 'http://themeteorbook.com'
+  }
 ];
 Template.postsList.helpers({
-	posts: postsData
-})
+  posts: postsData
+});
